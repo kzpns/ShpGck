5b0316d85fe8e4377b10f4f7b3fa7e7707a7ea4b
kzpns <kzpns0422@gmail.com>
2021-04-11 08:52:54 +0900

    Add function that returns cafecodes' byte[]

--- ShpGck/CafeCode/CafeCode.cs
+++ ShpGck/CafeCode/CafeCode.cs
@@ -1,9 +1,27 @@
-﻿namespace ShpGck.CafeCode
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShpGck.CafeCode
 {
     public abstract class CafeCode
     {
         public abstract byte GetCafeCodeID();
 
         public abstract uint[] ToRaw();
+
+        public byte[] ToBytes()
+        {
+            List<byte> codeBytes = new List<byte>();
+            foreach (uint line in ToRaw())
+            {
+                byte[] bytes = BitConverter.GetBytes(line);
+                if (BitConverter.IsLittleEndian)
+                {
+                    Array.Reverse(bytes);
+                }
+                codeBytes.AddRange(bytes);
+            }
+            return codeBytes.ToArray();
+        }
     }
 }
