ad386dbe5d8eedc8a2135ac89dbb75fbf33e2bcb
kzpns <kzpns0422@gmail.com>
2021-04-11 08:47:39 +0900

    Add somethings for TCP connection

---
+++ ShpGck/Tcp/TcpGeckoConnector.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ShpGck.Tcp
+{
+    public class TcpGeckoConnector : IGeckoConnector
+    {
+        static readonly int DefaultTcpPort = 7331;
+
+        private TcpClient BaseClient = new TcpClient();
+
+        private GeckoStream BaseStream;
+
+        public TcpGeckoConnector()
+        {
+
+        }
+
+        public void Connect(string server)
+        {
+            Connect(IPAddress.Parse(server), DefaultTcpPort);
+        }
+
+        public void Connect(IPAddress server)
+        {
+            Connect(server, DefaultTcpPort);
+        }
+
+        public void Connect(string server, int port)
+        {
+            Connect(IPAddress.Parse(server), port);
+        }
+
+        public void Connect(IPAddress server, int port)
+        {
+            BaseClient.Connect(server, port);
+        }
+
+        public void Disconnect()
+        {
+            if (BaseStream != null)
+            {
+                BaseStream.Close();
+            }
+            BaseClient.Close();
+        }
+
+        public GeckoStream GetStream()
+        {
+            if (BaseStream == null)
+            {
+                BaseStream = new TcpGeckoStream(BaseClient.GetStream());
+            }
+            return BaseStream;
+        }
+
+        public bool IsConnected => BaseClient.Connected;
+    }
+}
---
+++ ShpGck/Tcp/TcpGeckoStream.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Net.Sockets;
+
+namespace ShpGck.Tcp
+{
+    public class TcpGeckoStream : GeckoStream
+    {
+        private NetworkStream BaseStream;
+
+        public TcpGeckoStream(NetworkStream baseStream)
+        {
+            BaseStream = baseStream;
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+            => BaseStream.Read(buffer, offset, count);
+
+        public override void Write(byte[] buffer, int offset, int count)
+            => BaseStream.Write(buffer, offset, count);
+
+        public override void Flush() => BaseStream.Flush();
+
+        public override long Seek(long offset, SeekOrigin origin)
+            => BaseStream.Seek(offset, origin);
+
+        public override void SetLength(long value) => BaseStream.SetLength(value);
+
+        public override bool CanRead => BaseStream.CanRead;
+
+        public override bool CanWrite => BaseStream.CanWrite;
+
+        public override bool CanSeek => BaseStream.CanSeek;
+
+        public override long Position
+        {
+            get => BaseStream.Position;
+            set => BaseStream.Position = value;
+        }
+
+        public override long Length => BaseStream.Length;
+
+    }
+}
