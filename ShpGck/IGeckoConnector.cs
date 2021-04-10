3610d30434901f04879781adba14edca68b7aa9d
kzpns <kzpns0422@gmail.com>
2021-04-11 08:46:16 +0900

    Add IGeckoConnector for server connection

---
+++ ShpGck/IGeckoConnector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace ShpGck
+{
+    public interface IGeckoConnector
+    {
+        void Disconnect();
+
+        GeckoStream GetStream();
+
+        bool IsConnected
+        {
+            get;
+        }
+    }
+}
