4e60576d40319d4c721c4f2fc8716183858661a5
kzpns <kzpns0422@gmail.com>
2021-04-11 08:48:20 +0900

    Add CafeCodeHandler for managing cafe codes

---
+++ ShpGck/CafeCodeHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShpGck
+{
+    public class CafeCodeHandler
+    {
+        public static readonly GeckoMemoryAddress CafeCodeStartAddress = 
+            GeckoMemoryAddress.FromEffective(0x01133000);
+
+        public static readonly GeckoMemoryAddress CafeCodeEndAddress = 
+            GeckoMemoryAddress.FromEffective(0x01134300);
+
+        public static readonly GeckoMemoryAddress StatusAddress =
+            GeckoMemoryAddress.FromEffective(0x10014CFC);
+
+        private GeckoClient Client;
+
+        public CafeCodeHandler(GeckoClient client)
+        {
+            Client = client;
+        }
+
+        public void SetEnabled(bool status)
+        {
+            Client.Memory.WriteInt32(StatusAddress, Convert.ToInt32(status));
+        }
+
+        public void ClearCode()
+        {
+            SetEnabled(false);
+        }
+    }
+}
