2d5900e70350c5bc03ae48b02c7546bc90b5daaf
kzpns <kzpns0422@gmail.com>
2021-04-11 08:49:46 +0900

    Add some write functions for GeckoStream

---
+++ ShpGck/GeckoStream_Write.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ShpGck
+{
+    partial class GeckoStream
+    {
+        public void WriteInt8(sbyte value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            EndianizeBytes(bytes);
+            Write(bytes, 0, sizeof(sbyte));
+        }
+
+        public void WriteUInt8(byte value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            EndianizeBytes(bytes);
+            Write(bytes, 0, sizeof(byte));
+        }
+
+        public void WriteInt16(short value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            EndianizeBytes(bytes);
+            Write(bytes, 0, sizeof(short));
+        }
+
+        public void WriteUInt16(ushort value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            EndianizeBytes(bytes);
+            Write(bytes, 0, sizeof(ushort));
+        }
+
+        public void WriteInt32(int value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            EndianizeBytes(bytes);
+            Write(bytes, 0, sizeof(int));
+        }
+
+        public void WriteUInt32(uint value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            EndianizeBytes(bytes);
+            Write(bytes, 0, sizeof(uint));
+        }
+
+        public void WriteInt64(long value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            EndianizeBytes(bytes);
+            Write(bytes, 0, sizeof(long));
+        }
+
+        public void WriteUInt64(ulong value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            EndianizeBytes(bytes);
+            Write(bytes, 0, sizeof(ulong));
+        }
+
+        public void WriteBytes(byte[] value)
+        {
+            Write(value, 0, value.Length);
+        }
+
+        public void WriteCommand(GeckoCommand value)
+        {
+            byte[] bytes = GeckoConverter.GetBytes(value);
+            EndianizeBytes(bytes);
+            Write(bytes, 0, sizeof(GeckoCommand));
+        }
+    }
+}
