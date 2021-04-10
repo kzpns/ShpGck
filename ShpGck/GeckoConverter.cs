33f2c902bdec6fe41fda6a34955e2925d646cb7d
kzpns <kzpns0422@gmail.com>
2021-04-11 08:53:29 +0900

    Add GeckoConverter for converting some data

---
+++ ShpGck/GeckoConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShpGck
+{
+    public static class GeckoConverter
+    {
+        public static readonly bool IsLittleEndian = BitConverter.IsLittleEndian;
+
+        public static byte[] GetBytes(GeckoCommand value)
+        {
+            return new byte[]
+            {
+                (byte)value
+            };
+        }
+
+        public static GeckoCommand ToGeckoCommand(byte[] value, int startIndex)
+        {
+            return (GeckoCommand)value[startIndex + 0];
+        }
+    }
+}
